b79534f0e1947a4c9c8ec77a7739397efbede90e
Ronald DeBeras <rdeberas@gmail.com>
2018-02-05 14:52:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,6 @@
-function forLoop(array) {
-  for (var i = 0; i < array.length; i++) {
-    var temp = `I am ${i} strange loop`;
-    if(i > 1){
-      var temp += 's';
-    }
-
-  }
-  return array;
+function doWhileLoop(array) {
+do {array.pop();
+}
+while (array.length > 0 && maybeTrue());
+return array;
 }
